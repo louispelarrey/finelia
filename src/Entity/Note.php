a2b7dbfc5ea0913428db300e909d227147b8b9d4
Louis Pelarrey <l.pelarrey@adlperformance.fr>
2021-05-21 13:40:01 +0200

    entites

---
+++ src/Entity/Note.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NoteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=NoteRepository::class)
+ */
+class Note
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $note;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $coef;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNote(): ?float
+    {
+        return $this->note;
+    }
+
+    public function setNote(int $note): self
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    public function getCoef(): ?float
+    {
+        return $this->coef;
+    }
+
+    public function setCoef(float $coef): self
+    {
+        $this->coef = $coef;
+
+        return $this;
+    }
+}
